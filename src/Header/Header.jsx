1d53b3230ca38b4921339912f71328597acf0129
WAQAR2211011KHICND <mohammad.waqar@xloopdigital.com>
2022-12-07 15:33:12 +0500

    Header component Created

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header(){
+    return (   
+        <div className="headerH1">
+            <h1>My Goal for this Book</h1>  
+        </div>
+         )
+}   
+
+
+export default Header;
