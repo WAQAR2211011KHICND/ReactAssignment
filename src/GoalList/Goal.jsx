660429e5d7e598a8b7502c166aea09dbd85139a0
WAQAR2211011KHICND <mohammad.waqar@xloopdigital.com>
2022-12-07 15:38:58 +0500

    Third Goal createrd

--- src/GoalList/Goal.jsx
+++ src/GoalList/Goal.jsx
@@ -1,10 +1,12 @@
 import FirstGoal from './FirstGoal';
 import SecondGoal from './SecondGoal';
+import ThirdGoal from './ThirdGoal';
 
 function Goal(){
     return (<>
             <FirstGoal/>
             <SecondGoal/>
+            <ThirdGoal/>
     </>)
 }
 
---
+++ src/GoalList/ThirdGoal.jsx
@@ -0,0 +1,17 @@
+
+function ThirdGoal(){
+
+    return (
+    <div className="GoalHeading">
+        <ul>
+            <li>
+                 <h1>Motivate you to continue Learning</h1>
+            </li>
+        </ul>
+
+        <p>As a Developer, learning never ends. I want to ensure that my advance (React) resources after finished </p>
+    </div>
+    );
+}
+
+export default ThirdGoal;
